27b3bbb9ca3d6e7bedcfe3a6d568085fe32e5a12
Ahsan Hanif <ahsan@timeahead.co>
2023-08-17 16:05:45 +0500

    Updated Iway API key

--- src/shared/SearchLoading.tsx
+++ src/shared/SearchLoading.tsx
@@ -11,6 +11,12 @@ const SearchLoading: FC<SearchLoadingProps> = () => {
       <div className="grid grid-cols-1 gap-6 md:gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
          <CarSkeleton />
          <CarSkeleton />
+         <CarSkeleton />
+         <CarSkeleton />
+         <CarSkeleton />
+         <CarSkeleton />
+         <CarSkeleton />
+         <CarSkeleton />
       </div>
    );
 }
