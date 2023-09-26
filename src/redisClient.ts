6fefdeafc0972dac972d5f3d94c449bf409599ba
Ahsan Hanif <ahsan@timeahead.co>
2023-09-26 14:38:28 +0500

    changed upstash url instance url

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -1,5 +1,5 @@
 import Redis, { RedisOptions, Redis as RedisClient } from 'ioredis';
 
-const redisClient: RedisClient = new Redis("rediss://default:103383b069d24a48968ab39c296cb95b@cheerful-gecko-37606.upstash.io:37606");
+const redisClient: RedisClient = new Redis("rediss://default:a9125c69cd564e679d2272e8fada56b7@civil-heron-42015.upstash.io:42015");
 
 export default redisClient;
