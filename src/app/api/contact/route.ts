cd9a865766761a2f93039a6aa987eb46ac3c0b6a
Ahsan Hanif <ahsan@timeahead.co>
2023-09-28 14:37:00 +0500

    Adjusted next.config.js, added prisma for database

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { sql } from '@vercel/postgres';
+import prisma from '@/lib/prisma/client';
 import { isEmailValid } from '@/utils/random';
 
 export async function POST(request: Request) {
@@ -14,8 +14,15 @@ export async function POST(request: Request) {
          return NextResponse.json({ error: 'Invalid email format.' });
       }
 
-      await sql`CREATE TABLE IF NOT EXISTS user_contacts (id SERIAL PRIMARY KEY, full_name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, description TEXT, created_at TIMESTAMPTZ DEFAULT current_timestamp, updated_at TIMESTAMPTZ DEFAULT current_timestamp, search TEXT);`;
-      const result = await sql`INSERT INTO user_contacts (full_name, email, description, search) VALUES (${name}, ${email}, ${description}, ${JSON.stringify(search)});`;
+      const result = await prisma.user_contacts.create({
+         data: {
+            full_name: name,
+            email: email,
+            description: description,
+            search: JSON.stringify(search)
+         }
+      });
+      
       return NextResponse.json({ data: result });
    } catch (err) {
       return NextResponse.json({ error: 'An error occurred while processing the request.' });
--- src/app/api/locations/[search]/route.ts
+++ src/app/api/locations/[search]/route.ts
@@ -1,5 +1,4 @@
 import { NextResponse } from 'next/server';
-import db from '@/db.js';
 import axios from 'axios';
 
 export async function GET(request: Request, {params}: {params: {search: string}}){
@@ -28,19 +27,4 @@ export async function GET(request: Request, {params}: {params: {search: string}}
       
       return NextResponse.json({error: e.message});
    }
-
-   // try {
-      
-   //    const searchQuery = params.search;
-   //    if (!searchQuery || typeof searchQuery !== 'string') {
-   //       return NextResponse.json({ data: [] });
-   //    }
-   //    const query = `SELECT * FROM master_locations WHERE name ILIKE $1`;
-   //    const result = await db.query(query, [`%${searchQuery}%`]);
-      
-   //    return NextResponse.json({ data: result.rows });
-   // } catch (err) {
-
-   //    return NextResponse.json({ error: 'An error occurred while processing the request.' });
-   // }
 }
