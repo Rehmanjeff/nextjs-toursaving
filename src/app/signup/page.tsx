984cfc8f207c21014c1c6207ce8ae2d7027ea4e9
Ahsan Hanif <ahsan@timeahead.co>
2023-10-30 14:05:29 +0500

    Update IWAY API token

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,4 +1,6 @@
-import React, { FC } from "react";
+"use client";
+
+import React, { FC, useState } from "react";
 import facebookSvg from "@/images/Facebook.svg";
 import twitterSvg from "@/images/Twitter.svg";
 import googleSvg from "@/images/Google.svg";
@@ -7,6 +9,10 @@ import ButtonPrimary from "@/shared/ButtonPrimary";
 import Image from "next/image";
 import Link from "next/link";
 import { PathName } from "@/routers/types";
+import useNextRouter from "@/hooks/useNextRouter";
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from "react-hook-form";
 
 export interface PageSignUpProps {}
 
@@ -29,6 +35,71 @@ const loginSocials: any = [
 ];
 
 const PageSignUp: FC<PageSignUpProps> = ({}) => {
+
+   const [error, setError] = useState<string | false>(false);
+   const { redirectTo } = useNextRouter();
+   
+   const schema = yup.object().shape({
+      fullName: yup.string().max(40, 'Full name is too long').required('Full name is required'),
+      email: yup.string().email('Email provided is invalid').required('Email is required'),
+      emailConfirmation: yup.string()
+        .email('Email confirmation provided is invalid')
+        .oneOf([yup.ref('email')], 'Email confirmation must match the email')
+        .required('Email confirmation is required'),
+      password: yup.string().min(8, 'Password must be at least 8 characters long').required('Password is required'),
+      contactNumber: yup.string()
+         .matches(/^[0-9]+$/, 'Contact number must only contain numeric digits')
+         .min(10, 'Contact number is too short')
+         .max(15, 'Contact number is too long')
+         .required('Contact number required'),
+   });
+   
+   const { register, handleSubmit, formState: {errors}, reset } = useForm({
+      resolver: yupResolver(schema),
+   });
+   
+   const onSubmit = async (formData : any) => {
+      
+      const email = formData.email;
+      const password = formData.password;
+      const fullName = formData.fullName;
+      const contactNumber = formData.contactNumber;
+   
+      try {
+         
+         fetch('/api/user/signup', {
+            method: 'POST', 
+            headers: {'Content-Type': 'application/json'}, 
+            body: JSON.stringify({'search': search, 'lang': 'en', 'car' : car, trip: updatedTrip})
+         }).then((response) => response.json()).then((data) => {
+
+            setIsLoading(false);
+            if (data.response.error) {
+               throw data.response.error;
+            } else {
+               localStorage.removeItem('tour-checkout-vehicle');
+               localStorage.removeItem('tour-search');
+               localStorage.setItem('tour-booking-number', data.response.booking);
+               redirectTo('/pay-done' as any);
+            }
+         }).catch((error) => {
+
+            const err = getErrorMessage(error);
+            setIsLoading(false);
+            showNotification(err, 'error');
+            setTimeout(() => { hideNotification() }, 3000);
+         });
+      
+         if (result && result.error) {
+            setError('Error: ' + result.error);
+         } else {
+            redirectTo('/account');
+         }
+      } catch (error: any) {
+         setError('Error: ' + error.toString());
+      }
+   };
+   
    return (
       <div className={`nc-PageSignUp  `}>
          <div className="container mb-24 lg:mb-32">
@@ -56,22 +127,36 @@ const PageSignUp: FC<PageSignUpProps> = ({}) => {
             )}
             <form className="grid grid-cols-1 gap-6" action="#" method="post">
                <label className="block">
-               <span className="text-neutral-800 dark:text-neutral-200">
-                  Email address
-               </span>
-               <Input
-                  type="email"
-                  placeholder="example@example.com"
-                  className="mt-1"
-               />
+                  <span className="text-neutral-800 dark:text-neutral-200">
+                     Full name
+                  </span>
+                  <Input type="text" placeholder="" className="mt-1" error={errors.fullName ? errors.fullName.message : ''} {...register('fullName')} />
                </label>
                <label className="block">
-               <span className="flex justify-between items-center text-neutral-800 dark:text-neutral-200">
-                  Password
-               </span>
-               <Input type="password" className="mt-1" />
+                  <span className="text-neutral-800 dark:text-neutral-200">
+                     Email address
+                  </span>
+                  <Input type="text" placeholder="" className="mt-1" error={errors.email ? errors.email.message : ''} {...register('email')} />
                </label>
-               <ButtonPrimary type="submit">Continue</ButtonPrimary>
+               <label className="block">
+                  <span className="text-neutral-800 dark:text-neutral-200">
+                     Confirm your email
+                  </span>
+                  <Input type="text" placeholder="" className="mt-1" error={errors.emailConfirmation ? errors.emailConfirmation.message : ''} {...register('emailConfirmation')} />
+               </label>
+               <label className="block">
+                  <span className="text-neutral-800 dark:text-neutral-200">
+                     Contact number
+                  </span>
+                  <Input type="text" placeholder="" className="mt-1" error={errors.contactNumber ? errors.contactNumber.message : ''} {...register('contactNumber')} />
+               </label>
+               <label className="block">
+                  <span className="flex justify-between items-center text-neutral-800 dark:text-neutral-200">
+                     Password
+                  </span>
+                  <Input type="password" className="mt-1" error={errors.password ? errors.password.message : ''} {...register('password')} />
+               </label>
+               <ButtonPrimary type="button" onClick={handleSubmit(onSubmit)}>Continue</ButtonPrimary>
             </form>
 
             {/* ==== */}
